7168bbcc21014fe1141602455e0d44655e88f19e
Dwayne_Spades <dwaynespades@gmail.com>
2021-09-19 12:41:23 -0500

    added sprite Editor package and added plant unit sprites

---
+++ Power_Pumpkin_Defense/Assets/scripts/sprites_and_art/faceCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class faceCamera : MonoBehaviour
+{
+
+    public bool zRotate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Camera.main.transform);
+        if (zRotate)
+            transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
+
+    }
+}
---
+++ Power_Pumpkin_Defense/Assets/scripts/sprites_and_art/mouseAim.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseAim : MonoBehaviour
+{
+
+
+
+    public Vector3 targetPosition;
+    public Vector3 newDir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void OnDrawGizmos()
+    {
+       
+
+        Gizmos.DrawSphere(targetPosition, 0.5f);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Ray r = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(r, out hit, 100.0f))
+        {
+            //draw invisible ray cast/vector
+            Debug.DrawLine(r.origin, hit.point,Color.yellow);
+        }
+        targetPosition = hit.point;
+
+        RaycastHit hit2;
+
+        newDir =  hit.point-transform.position;
+        newDir.Normalize();
+
+        if (Physics.Raycast(transform.position,newDir, out hit2,100.0f))
+        {
+            //draw invisible ray cast/vector
+            Debug.DrawLine(transform.position, hit2.point,Color.red);
+            
+        }
+
+        //Vector3 mouseRayPos = Ray()
+    }
+}
